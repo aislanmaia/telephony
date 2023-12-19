679e51911e2eb204c2ae97f449973eda9ba1f3e3
Aislan Maia <aislan.maia@totvs.com.br>
2023-12-19 00:11:32 -0300

    feat: add prepaid and pospaid

--- test/telephony/core/subscriber_test.exs
+++ test/telephony/core/subscriber_test.exs
@@ -1,12 +1,13 @@
 defmodule Telephony.Core.SubscriberTest do
   use ExUnit.Case
-  alias Telephony.Core.Subscriber
+  alias Telephony.Core.{Subscriber, Prepaid, Pospaid}
 
-  test "create a subscriber" do
+  test "create a prepaid subscriber" do
     # Given
     payload = %{
       full_name: "Aislan",
-      phone_number: "123"
+      phone_number: "123",
+      subscriber_type: :prepaid
     }
 
     # When
@@ -15,7 +16,25 @@ defmodule Telephony.Core.SubscriberTest do
     expect = %Subscriber{
       full_name: "Aislan",
       phone_number: "123",
-      subscriber_type: :prepaid
+      subscriber_type: %Prepaid{credits: 0, recharges: []}
+    }
+
+    assert expect == result
+  end
+
+  test "create a pospaid subscriber" do
+    payload = %{
+      full_name: "Aislan",
+      phone_number: "123",
+      subscriber_type: :pospaid
+    }
+
+    result = Subscriber.new(payload)
+
+    expect = %Subscriber{
+      full_name: "Aislan",
+      phone_number: "123",
+      subscriber_type: %Pospaid{spent: 0}
     }
 
     assert expect == result
