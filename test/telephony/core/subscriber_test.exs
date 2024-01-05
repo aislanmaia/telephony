21592da912745d60363e5fbb9555934fd69fbdcf
Aislan Maia <aislan.maia@totvs.com.br>
2024-01-05 12:12:14 -0300

    feat: implement subscriber responsabilities

--- test/telephony/core/subscriber_test.exs
+++ test/telephony/core/subscriber_test.exs
@@ -1,6 +1,6 @@
 defmodule Telephony.Core.SubscriberTest do
   use ExUnit.Case
-  alias Telephony.Core.{Call, Pospaid, Prepaid, Recharge, Subscriber}
+  alias Telephony.Core.{Pospaid, Prepaid, Subscriber}
 
   setup do
     pospaid = %Subscriber{
@@ -28,14 +28,15 @@ defmodule Telephony.Core.SubscriberTest do
 
     # When
     result = Subscriber.new(payload)
-    # Then
-    expect = %Subscriber{
+
+    # then
+    expected = %Subscriber{
       full_name: "Aislan",
       phone_number: "123",
       subscriber_type: %Prepaid{credits: 0, recharges: []}
     }
 
-    assert expect == result
+    assert expected == result
   end
 
   test "create a pospaid subscriber" do
@@ -47,81 +48,12 @@ defmodule Telephony.Core.SubscriberTest do
 
     result = Subscriber.new(payload)
 
-    expect = %Subscriber{
+    expected = %Subscriber{
       full_name: "Aislan",
       phone_number: "123",
       subscriber_type: %Pospaid{spent: 0}
     }
 
-    assert expect == result
-  end
-
-  test "make a pospaid call", %{pospaid: pospaid} do
-    date = NaiveDateTime.utc_now()
-    result = Subscriber.make_call(pospaid, 2, date)
-
-    expect = %Subscriber{
-      full_name: "Aislan",
-      phone_number: "123",
-      subscriber_type: %Pospaid{spent: 2.08},
-      calls: [
-        %Call{
-          time_spent: 2,
-          date: date
-        }
-      ]
-    }
-
-    assert expect == result
-  end
-
-  test "make a prepaid call", %{prepaid: prepaid} do
-    date = NaiveDateTime.utc_now()
-    result = Subscriber.make_call(prepaid, 2, date)
-
-    expect = %Subscriber{
-      calls: [
-        %Call{
-          date: date,
-          time_spent: 2
-        }
-      ],
-      full_name: "Aislan",
-      phone_number: "123",
-      subscriber_type: %Prepaid{
-        credits: 7.1,
-        recharges: []
-      }
-    }
-
-    assert expect == result
-  end
-
-  test "make a recharge", %{prepaid: prepaid} do
-    date = NaiveDateTime.utc_now()
-    result = Subscriber.make_recharge(prepaid, 2, date)
-
-    expect = %Subscriber{
-      calls: [],
-      full_name: "Aislan",
-      phone_number: "123",
-      subscriber_type: %Prepaid{
-        credits: 12,
-        recharges: [
-          %Recharge{value: 2, date: date}
-        ]
-      }
-    }
-
-    assert expect == result
-  end
-
-  test "throw error when is not a prrepaid", %{pospaid: pospaid} do
-    date = NaiveDateTime.utc_now()
-    result = Subscriber.make_recharge(pospaid, 2, date)
-
-    expect = {:error, "Only prepaid can make a recharge"}
-
-    assert expect == result
+    assert expected == result
   end
 end
