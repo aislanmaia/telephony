fc8e881163f7bb15f79a687770a6cdafb6709042
Aislan Maia <aislan.maia@totvs.com.br>
2024-01-05 10:43:39 -0300

    feat: implement pospaid invoice print

--- lib/telephony/core/pospaid.ex
+++ lib/telephony/core/pospaid.ex
@@ -1,5 +1,6 @@
 defmodule Telephony.Core.Pospaid do
   alias Telephony.Core.Call
+  alias Telephony.Core.Invoice
   defstruct spent: 0
   @price_per_minute 1.04
 
@@ -21,5 +22,30 @@ defmodule Telephony.Core.Pospaid do
   def add_new_call(subscriber, time_spent, date) do
     call = Call.new(time_spent, date)
     %{subscriber | calls: subscriber.calls ++ [call]}
+  end
+
+  defimpl Invoice, for: Telephony.Core.Pospaid do
+    @price_per_minute 1.04
+
+    def print(_pospaid, calls, year, month) do
+      calls =
+        Enum.reduce(calls, [], fn call, acc ->
+          if call.date.year == year and call.date.month == month do
+            value_spent = call.time_spent * @price_per_minute
+            call = %{date: call.date, time_spent: call.time_spent, value_spent: value_spent}
+
+            acc ++ [call]
+          else
+            acc
+          end
+        end)
+
+      value_spent = Enum.reduce(calls, 0, &(&1.value_spent + &2))
+
+      %{
+        value_spent: value_spent,
+        calls: calls
+      }
+    end
   end
 end
--- test/telephony/core/pospaid_test.exs
+++ test/telephony/core/pospaid_test.exs
@@ -1,6 +1,6 @@
 defmodule Telephony.Core.PospaidTest do
   use ExUnit.Case
-  alias Telephony.Core.{Call, Pospaid, Subscriber}
+  alias Telephony.Core.{Call, Invoice, Pospaid, Subscriber}
 
   setup do
     subscriber = %Subscriber{
@@ -31,5 +31,51 @@ defmodule Telephony.Core.PospaidTest do
     }
 
     assert expect == result
+  end
+
+  test "print invoice" do
+    date = ~D[2023-12-23]
+    last_month = ~D[2023-11-23]
+
+    subscriber = %Subscriber{
+      full_name: "Aislan",
+      phone_number: "123",
+      subscriber_type: %Pospaid{spent: 90 * 1.04},
+      calls: [
+        %Call{
+          time_spent: 10,
+          date: date
+        },
+        %Call{
+          time_spent: 50,
+          date: last_month
+        },
+        %Call{
+          time_spent: 30,
+          date: last_month
+        }
+      ]
+    }
+
+    subscriber_type = subscriber.subscriber_type
+    calls = subscriber.calls
+
+    expect = %{
+      value_spent: 80 * 1.04,
+      calls: [
+        %{
+          time_spent: 50,
+          date: last_month,
+          value_spent: 50 * 1.04
+        },
+        %{
+          time_spent: 30,
+          date: last_month,
+          value_spent: 30 * 1.04
+        }
+      ]
+    }
+
+    assert expect == Invoice.print(subscriber_type, calls, 2023, 11)
   end
 end
