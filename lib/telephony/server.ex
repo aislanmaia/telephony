daff6643a3008b5623fa69e64dee8fe8e4f23eed
Aislan Maia <aislan.sousamaia@gmail.com>
2024-01-17 01:22:24 -0300

    feat: finish server implementation

--- lib/telephony/server.ex
+++ lib/telephony/server.ex
@@ -14,4 +14,33 @@ defmodule Telephony.Server do
       subscribers -> {:reply, subscribers, subscribers}
     end
   end
+
+  def handle_call({:search_subscriber, phone_number}, _from, subscribers) do
+    subscriber = Core.search_subscriber(subscribers, phone_number)
+    {:reply, subscriber, subscribers}
+  end
+
+  def handle_call({:make_call, phone_number, time_spent, date}, _, subscribers) do
+    case Core.make_call(subscribers, phone_number, time_spent, date) do
+      {:error, _} = err -> {:reply, err, subscribers}
+      {subscribers, subscriber} -> {:reply, subscriber, subscribers}
+    end
+  end
+
+  def handle_call({:print_invoice, phone_number, year, month}, _, subscribers) do
+    invoice = Core.print_invoice(subscribers, phone_number, year, month)
+    {:reply, invoice, subscribers}
+  end
+
+  def handle_call({:print_all_invoices, year, month}, _, subscribers) do
+    invoices = Core.print_all_invoices(subscribers, year, month)
+    {:reply, invoices, subscribers}
+  end
+
+  def handle_cast({:make_recharge, phone_number, value, date}, subscribers) do
+    case Core.make_recharge(subscribers, phone_number, value, date) do
+      {:error, _} -> {:noreply, subscribers}
+      {subscribers, _} -> {:noreply, subscribers}
+    end
+  end
 end
---
+++ test/telephony/server_test.exs
@@ -0,0 +1,120 @@
+defmodule Telephony.ServerTest do
+  use ExUnit.Case
+  alias Telephony.Server
+
+  setup do
+    {:ok, pid} = Server.start_link(:test)
+
+    payload = %{
+      full_name: "Aislan",
+      type: :prepaid,
+      phone_number: "123"
+    }
+
+    %{pid: pid, process_name: :test, payload: payload}
+  end
+
+  test "check telephony subscribers state", %{pid: pid} do
+    assert [] = :sys.get_state(pid)
+  end
+
+  test "create a subscriber", %{pid: pid, process_name: process_name, payload: payload} do
+    old_state = :sys.get_state(pid)
+    assert [] == old_state
+
+    result = GenServer.call(process_name, {:create_subscriber, payload})
+
+    refute old_state == result
+
+    assert [
+             %Telephony.Core.Subscriber{
+               full_name: "Aislan",
+               phone_number: "123",
+               type: %Telephony.Core.Prepaid{credits: 0, recharges: []},
+               calls: []
+             }
+           ] == result
+  end
+
+  test "error message when try to create a subscriber", %{
+    pid: pid,
+    process_name: process_name,
+    payload: payload
+  } do
+    old_state = :sys.get_state(pid)
+    assert [] == old_state
+
+    GenServer.call(process_name, {:create_subscriber, payload})
+    result = GenServer.call(process_name, {:create_subscriber, payload})
+
+    assert {:error, "Subscriber `123` already exists"} == result
+  end
+
+  test "search subscriber", %{process_name: process_name, payload: payload} do
+    GenServer.call(process_name, {:create_subscriber, payload})
+    result = GenServer.call(process_name, {:search_subscriber, payload.phone_number})
+    assert result.full_name == payload.full_name
+  end
+
+  test "make a recharge", %{pid: pid, process_name: process_name, payload: payload} do
+    GenServer.call(process_name, {:create_subscriber, payload})
+
+    date = Date.utc_today()
+    state = :sys.get_state(pid)
+    subscriber_state = hd(state)
+    assert subscriber_state.type.recharges == []
+    :ok = GenServer.cast(process_name, {:make_recharge, payload.phone_number, 100, date})
+
+    state = :sys.get_state(pid)
+    subscriber_state = hd(state)
+    refute subscriber_state.type.recharges == []
+  end
+
+  test "make a success call", %{pid: pid, process_name: process_name, payload: payload} do
+    date = Date.utc_today()
+    phone_number = payload.phone_number
+    time_spent = 10
+
+    GenServer.call(process_name, {:create_subscriber, payload})
+    GenServer.cast(process_name, {:make_recharge, payload.phone_number, 100, date})
+
+    state = :sys.get_state(pid)
+    subscriber_state = hd(state)
+    assert subscriber_state.calls == []
+
+    result = GenServer.call(process_name, {:make_call, phone_number, time_spent, date})
+
+    refute result.calls == []
+  end
+
+  test "make a failed call", %{process_name: process_name, payload: payload} do
+    date = Date.utc_today()
+    phone_number = payload.phone_number
+    time_spent = 10
+
+    GenServer.call(process_name, {:create_subscriber, payload})
+
+    result = GenServer.call(process_name, {:make_call, phone_number, time_spent, date})
+
+    assert result == {:error, "Subscriber doest not have credits"}
+  end
+
+  test "print invoice", %{process_name: process_name, payload: payload} do
+    GenServer.call(process_name, {:create_subscriber, payload})
+    date = Date.utc_today()
+    phone_number = payload.phone_number
+
+    result = GenServer.call(process_name, {:print_invoice, phone_number, date.year, date.month})
+
+    assert result.invoice.calls == []
+  end
+
+  test "print all invoices", %{process_name: process_name, payload: payload} do
+    GenServer.call(process_name, {:create_subscriber, payload})
+    date = Date.utc_today()
+
+    result = GenServer.call(process_name, {:print_all_invoices, date.year, date.month})
+
+    assert result |> hd() |> then(& &1.invoice) == %{credits: 0, recharges: [], calls: []}
+  end
+end
